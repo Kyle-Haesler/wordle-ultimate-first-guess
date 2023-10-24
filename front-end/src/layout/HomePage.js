44599cc150ef25c825db1b0d5e6501e37b915213
Kyle Haesler <kyle.haesler@gmail.com>
2023-10-24 12:32:17 -0400

    Made some cosmetic changes and added description and footer

--- front-end/src/layout/HomePage.js
+++ front-end/src/layout/HomePage.js
@@ -20,9 +20,28 @@ function HomePage() {
 
   return (
         <div>
+          <header>
             <h1 className="display-4 bg-success text-white p-3">Wordle's Ultimate First Guess</h1>
-      <h2>{firstGuess}</h2>
+            </header>
+            <br />
+            <main className="bg-light">
+      <h3 className="text-decoration-underline">Today's Start Word:</h3>
+      <h4 className="text-success font-italic">{!firstGuess.length ? "...Have you ever fried cheese in a pan?" : firstGuess}</h4> 
       <ErrorAlert error={firstGuessError} />
+      </main>
+      <hr />
+      <article>
+        <h5>How Does This Work?</h5>
+        <p>Wordle has a list of words that could be solutions for any given day (A). Each day, a solution is provided. Thus, we have a list of all words used as solutions (B). If we subtract (B) from (A), we are left with all words that have the potential to be solutions (C).</p>
+        <p>With (C), we count the letter frequency of each letter of each word included in (C). With our tally of letter counts, we assign each word in (C) a score by summing each letter's frequency of each word.</p>
+        <p>We select the highest scoring word as our start word for each day. This start word gives you the highest probability of getting the most letters (yellow).</p>
+        <p>This will automatically calculate everytime you come to this site so be sure to give it a look before you play!</p>
+      </article>
+      <hr />
+      <footer className="display-5 bg-success text-white p-3">
+        <h6>Copyright: Kyle Haesler</h6>
+        <p>In honor of the Worlding Turtles</p>
+      </footer>
       </div>
   );
 }
